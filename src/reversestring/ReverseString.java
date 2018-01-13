f1c5678ecdabeb89b6f01871e073809a4a5be02d
Jaspreet Singh <>
2018-01-12 21:04:38 -0500

    reverse and print the string

--- src/reversestring/ReverseString.java
+++ src/reversestring/ReverseString.java
@@ -17,16 +17,20 @@ public class ReverseString {
     public static void main(String[] args) {
         // TODO code application logic here
      String inStr;  //input the string 
-     int strlen;    //for the length of the string
+     int strlen;
+     String reversedString=""; //for the length of the string
      Scanner in = new Scanner(System.in);
         System.out.println("Enter the string you want to reversed");
         inStr=in.next();     //read the string 
         strlen=inStr.length();   //calculate the length of the string 
         
-      for(int i=strlen-1;i>=0;i++){
-          //to reverse the string
-                                         
+      for(int i=strlen-1;i>=0;i--){
+        //to reverse the string
+     reversedString=reversedString+ inStr.charAt(i); //reverse string character by character
+
       }
+      System.out.println("The reversed string is "+reversedString); //to print out the reversed string
+      
     }
     
 }
